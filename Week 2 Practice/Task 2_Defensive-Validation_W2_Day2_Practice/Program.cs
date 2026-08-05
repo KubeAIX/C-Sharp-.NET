e37fc73deb96c2826c88a2d573d81664724dfe7c
LEGACY-MODE-PC2000 <LEGACY-MODE-PC2000@DESKTOP-J0NKKGR>
2026-08-05 15:43:16 +0500

    Week 2 Task 2 Defensive Validation concepts and example of Student Records

---
+++ Week 2 Practice/Task 2_Defensive-Validation_W2_Day2_Practice/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    //for stdunts record yahan dic use karengy Keys unique hoti hain, values duplicate ho sakti hain.
+    {           // key, Value
+        Dictionary<int, string> students = new Dictionary<int, string>();
+        // here ARID University Registration Numbers are used as keys and student names as values
+        students.Add(4669, "Abdul Ahad");
+        students.Add(4668, "Abdul Saboor");
+        students.Add(4680, "Maria Zahid");
+        students.Add(4676, "Umar Tahir");
+        Console.WriteLine("Enter your Registration Number: ");
+        // use 
+        int id = Convert.ToInt32(Console.ReadLine());
+
+        //Defensive Validation
+
+        if (!students.ContainsKey(id))
+        {
+            Console.WriteLine("Student Not found, yours data not available contact Admin");
+            return;
+
+        }  // use concatination to show the student name
+
+        Console.WriteLine("StudentName: " + students[id]);
+
+
+    }
+     
+}
