fad816160dc9fc22d9f38ae0465d783adb96d881
LEGACY-MODE-PC2000 <LEGACY-MODE-PC2000@DESKTOP-J0NKKGR>
2026-08-19 17:58:39 +0500

    Task 2 Week 4 (Implement employee create, read, update and delete endpoints.)

---
+++ Week 4 Practice/Web_API_Project_2/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web_API_Project_2.Models;
+
+namespace Web_API_Project_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        //create
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return Ok("Employe Created");
+        }
+
+        //Read
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("Get All Employees");
+
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Update([FromRoute] int id)
+        {
+            return Ok($"Employee {id} updated");
+        }
+
+        // DELETE
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            return Ok($"Employee {id} deleted");
+        }
+
+    }
+}
---
+++ Week 4 Practice/Web_API_Project_2/Models/Employee.cs
@@ -0,0 +1,6 @@
+﻿namespace Web_API_Project_2.Models
+{
+    public class Employee
+    {
+    }
+}
